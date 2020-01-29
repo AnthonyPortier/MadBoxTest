1c178dab4ade424848da8a44a2e97c4bed8e9542
Anthony Portier <anthonynicolasportier@gmail.com>
2020-01-29 17:24:49 +0100

    set up du front ok

---
+++ front/src/components/Accueil.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import axios from 'axios'
+
+
+
+const Accueil =()=>{
+
+    return(
+        <div>
+            <p>OK</p>
+        </div>
+    )
+}
+
+export default Accueil;
