23fa26e5f90d80f7acab486a0e05dd75741f4467
Anthony Portier <anthonynicolasportier@gmail.com>
2020-01-29 11:36:04 +0100

    push suite à un merge de branch

---
+++ index.js
@@ -0,0 +1,19 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const models = require('./models')
+// const admin = require('./routes/index')
+app.use(express.json())
+app.use(cors())
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+
+const port = process.env.PORT
+
+
+models
+    .sequelize
+    .sync()
+    .then(() => app.listen(port, console.log('Server is Running')))
